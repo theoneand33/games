df2fc8c160b262858c309605b4f85c2ff4946c40
scnew8013 <scnew8013@gmail.com>
2025-07-22 00:43:57 +0000

    revert "add search box"

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({});
     vite: {
         build: {
-      // Run right after Astro finishes
-          rollupOptions: {
-            output: {
-            plugins: [{
-                name: "make-search-index",
-                generateBundle() {
-              // import() runs it in Node context
-                import("src/scripts/make-search-index.js");
-            }
-          }]
-        }
-      }
     }
   }
