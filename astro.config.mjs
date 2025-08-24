f43d195b9ed1c2bb6ed29476641ed6517b7aafbd
scnew8013 <scnew8013@gmail.com>
2025-08-24 21:08:30 +0000

    add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
-export default defineConfig({});
-    vite: {
-        build: {
-    }
-  }
+export default defineConfig({
+  integrations: [react()]
+});
+vite: {
+    build: {
+}
+}
