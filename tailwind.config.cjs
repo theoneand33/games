2cc69918c2afef8d0d483e9fc9d4299317629cab
scnew8013 <scnew8013@gmail.com>
2025-09-03 21:23:41 +0000

    fix height and width

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  ],
+  theme: {
+    extend: {
+      width: {
+        '300': '300px',
+      },
+      height: {
+        '200': '200px',
+      },
+    },
+  },
+  plugins: [],
+}
