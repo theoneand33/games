d5fe5ea0c4b762003d55dc5af302976605e08ede
theoneand33 <120695243+theoneand33@users.noreply.github.com>
2025-10-13 09:13:09 +1300

    maybe fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,14 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   // Add your site URL here - replace with your actual domain
   site: "https://unblocked-games.vercel.app/",
 
-  integrations: [sitemap()],
+  integrations: [sitemap(), react()],
   vite: {
     plugins: [tailwindcss()],
   },
