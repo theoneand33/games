ce12643f0ca63b66609bfa093c46db0567b5f1fc
theoneand33 <120695243+theoneand33@users.noreply.github.com>
2026-06-11 11:11:01 +1200

    chore: remove unused packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,11 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   site: "https://unblocked-games.vercel.app/",
-  integrations: [sitemap(), react()],
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
