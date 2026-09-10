c55fc9e007af710ddfc304f33da5071d158a92f6
theoneand33 <theoneand33@users.noreply.github.com>
2026-09-11 10:42:56 +1200

    chore(content): update game seo and homepage default list
    - Add "how to play" guides to popular games
    - Promote binding of isaac, meat boy, and mutilate a doll 2 to default games
    - Add redirects for games with trailing slashes

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,6 +15,13 @@ export default defineConfig({
     "/games/run3": "/games/run-3",
     "/games/gun-mayhem2": "/games/gun-mayhem-2",
     "/games/geography-game": "/games/geography-game-usa",
+    "/games/tetris/": "/games/tetris",
+    "/games/clicker-heroes/": "/games/clicker-heroes",
+    "/games/fleeing-the-complex/": "/games/fleeing-the-complex",
+    "/games/breaking-the-bank/": "/games/breaking-the-bank",
+    "/games/hobo-5-space-brawls/": "/games/hobo-5-space-brawls",
+    "/games/super-mario-flash/": "/games/super-mario-flash",
+    "/games/strike-force-heroes/": "/games/strike-force-heroes",
   },
   integrations: [
     sitemap({
